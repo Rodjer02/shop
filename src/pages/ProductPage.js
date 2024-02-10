182b5f904fcfcf5460a2eb8ee8ca9802757366dc
Rodjer02 <azamatkoyanbaev02@gmail.com>
2024-02-10 22:35:48 +0600

    Забыл добавить класс container

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -9,20 +9,22 @@ const ProductPage = () => {
   if (isLoading) return null;
   console.log(data);
   return (
-    <div className="product-item">
-      <div className="product-img">
-        <img src={data.image} />
-      </div>
-      <div className="product-body">
-        <h2 className="product-title">{data.title}</h2>
-        <div className="product-desc">{data.description}</div>
-        <div className="product-body-bottom">
-          <b className="price">Price: {data.price}$</b>
-          <div
-            className="product-add-to-card hover"
-            onClick={() => addToOrders(data)}
-          >
-            Add to Cart
+    <div className="container">
+      <div className="product-item">
+        <div className="product-img">
+          <img src={data.image} />
+        </div>
+        <div className="product-body">
+          <h2 className="product-title">{data.title}</h2>
+          <div className="product-desc">{data.description}</div>
+          <div className="product-body-bottom">
+            <b className="price">Price: {data.price}$</b>
+            <div
+              className="product-add-to-card hover"
+              onClick={() => addToOrders(data)}
+            >
+              Add to Cart
+            </div>
           </div>
         </div>
       </div>
